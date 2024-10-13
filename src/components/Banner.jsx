ab66cda15c1c6bd17cb4a9a38601b1f54488fd12
meirelesgc <geu_costa@outlook.com>
2024-10-13 10:15:24 -0300

    att 13/10

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,37 @@
-import { Button, Card, Flex, Typography } from "antd";
 import React from "react";
-
-// <Typography.Title level={2} strong>
-//     {getTitle()} 
-// </Typography.Title>
-// <Typography.Text type="secondary" strong>
-//     texto referente a {selectedItem}
-// </Typography.Text>
+import { Button, Card, Flex, Typography } from "antd";
+import { CheckCircleOutlined, CloseCircleOutlined, HourglassOutlined } from "@ant-design/icons";
 
 const ArchiveCard = () => (
     <>
-        <Typography.Title level={2} strong>
-            Arquivo
-        </Typography.Title>
-        <Typography.Text type="secondary" strong>
-            Texto do arquivo
-        </Typography.Text>
+        <Flex vertical gap="middle">
+            <Typography.Title level={2} strong>
+                Adicione e gerencie os documentos
+            </Typography.Title>
+
+            <Typography.Text type="secondary" strong>
+                Acompanhe aqui os diferentes estágios do processamento
+            </Typography.Text>
+
+            <Flex wrap="nowrap" gap="large">
+                <Flex>
+                    <Card><HourglassOutlined /></Card>
+                    <Card>STANDBY</Card>
+                </Flex>
+                <Flex>
+                    <Card><CheckCircleOutlined /></Card>
+                    <Card>DONE</Card>
+                </Flex>
+                <Flex>
+                    <Card><CloseCircleOutlined /></Card>
+                    <Card>FAILED</Card>
+                </Flex>
+                <Flex>
+                    <Card><HourglassOutlined /></Card>
+                    <Card>IN-PROCESS</Card>
+                </Flex>
+            </Flex>
+        </Flex>
     </>
 );
 
@@ -58,10 +74,6 @@ const Banner = ({ selectedItem }) => {
             <Flex vertical gap='30px'>
                 <Flex vertical align="flex-start">
                     {getTitle()}
-                </Flex>
-                <Flex gap='large'>
-                    <Button type="primary" size="large">botão 9763</Button>
-                    <Button size="large">botão 5980</Button>
                 </Flex>
             </Flex>
         </Card>
