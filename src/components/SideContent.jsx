aee04e6e7ab0c3f914e1f2d1beef3a8f94d7ab15
meirelesgc <geu_costa@outlook.com>
2024-10-23 20:15:39 -0300

    ...

--- src/components/SideContent.jsx
+++ src/components/SideContent.jsx
@@ -1,14 +1,34 @@
-import { Flex } from "antd";
 import React from "react";
+import { Flex, Statistic, Row, Col } from 'antd';
 import ContentSidebar from "./ContentSidebar";
-import SideContentRouter from './SideContentRouter'
+import { useParams } from "react-router-dom";
 
-const SideContent = (selectedItem, setSelectedItem) => {
-    return (
-        <Flex vertical gap="2.3rem" style={{ width: 350 }}>
-            <ContentSidebar />
-            <SideContentRouter selectedItem={selectedItem} />
-        </Flex>
-    )
+const SideContent = () => {
+    const { document_id } = useParams();
+    const jsonData = {
+        CPF: 123,
+        Sacarose: 123,
+        'Ano da carreira': 123,
+    };
+    const renderSideContent = () => {
+        if (document_id) {
+            console.log(document_id)
+            return <Row gutter={[16, 16]}>
+                {[jsonData].map(([key, value], index) => (
+                    <Col span={8} key={index}>
+                        <Statistic
+                            title={key}
+                            value={value !== null ? value : 'N/A'}
+                        />
+                    </Col>
+                ))}
+            </Row>;
+        }
+    }
+
+    return <Flex vertical gap="2.3rem" style={{ width: 350 }}>
+        <ContentSidebar />
+        {renderSideContent()}
+    </Flex>
 }
 export default SideContent;
--- src/components/SideContentRouter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageContent = () => {
-    return <div>Page Not Found</div>;
-};
-
-export default PageContent;
