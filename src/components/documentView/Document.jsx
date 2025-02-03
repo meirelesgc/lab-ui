6178c2cfb999f0cae791ca719a5a6822049a74c8
meirelesgc <geu_costa@outlook.com>
2025-02-03 08:06:58 -0300

    ...

--- src/components/documentView/Document.jsx
+++ src/components/documentView/Document.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Document = ({ document_id }) => {
-    return <iframe src={'http://localhost:8000/document/file/' + document_id} width="100%" height="100%" title="PDF"
+    return <iframe src={'http://150.164.52.61:5173/api/document/file/' + document_id} width="100%" height="100%" title="PDF"
         style={{ border: 'none', margin: 0, padding: 0, overflow: 'hidden' }}
     />;
 };
